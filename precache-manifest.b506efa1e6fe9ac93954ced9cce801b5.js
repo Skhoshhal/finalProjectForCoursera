6bacccb7fd85ca83648f2350d43261b9eb68a9ca
Sayed Sadat <khoshhalsadat945@gmail.com>
2019-09-06 16:25:05 +0200

    Updates

---
+++ precache-manifest.b506efa1e6fe9ac93954ced9cce801b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f875580ec78e6b90a3c7fb97d45a5bcb",
+    "url": "/finalProjectForCoursera/index.html"
+  },
+  {
+    "revision": "1283c701275cc4e96419",
+    "url": "/finalProjectForCoursera/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4ba1b47422e2e3f96643",
+    "url": "/finalProjectForCoursera/static/css/main.edad0e56.chunk.css"
+  },
+  {
+    "revision": "1283c701275cc4e96419",
+    "url": "/finalProjectForCoursera/static/js/2.f1b625fe.chunk.js"
+  },
+  {
+    "revision": "4ba1b47422e2e3f96643",
+    "url": "/finalProjectForCoursera/static/js/main.bf278372.chunk.js"
+  },
+  {
+    "revision": "ca53e497e835156ddcfd",
+    "url": "/finalProjectForCoursera/static/js/runtime~main.db0bfde7.js"
+  },
+  {
+    "revision": "cb1540bbe962635786bb87e289f03f79",
+    "url": "/finalProjectForCoursera/static/media/spreadshop_logo.cb1540bb.svg"
+  }
+]);
